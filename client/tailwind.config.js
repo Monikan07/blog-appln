bc6564bd89c93e5714b2c7020a0d53a806475e61
Monikan07 <monikajm137@gmail.com>
2025-10-15 08:43:28 +0530

    Add files via upload

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,24 @@ import flowbite from 'flowbite/plugin';
 import tailwindScrollbar from 'tailwind-scrollbar';
 
 export default {
+  darkMode: 'class',
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       'edu': ['"Edu NSW ACT Cursive"', 'cursive', 'sans-serif'],
+        
+        'playwrite': ['"Playwrite DE SAS"', 'cursive', 'sans-serif'],
+        
+        'updock': ['Updock', 'cursive', 'sans-serif'],
+        
+        'bitcount': ['"Bitcount Grid Single Ink"', 'monospace', 'sans-serif'],
+      },
+    },
   },
   plugins: [flowbite, tailwindScrollbar],
 };
